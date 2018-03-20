1d3dc8bc1bd2b1d3545d1f729490194b353ffb48
chihwen <chihwen@doubleservice.com>
2018-03-20 14:50:38 +0800

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -24,6 +26,14 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
