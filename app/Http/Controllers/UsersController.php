10439c0219528babd94cf54925fcc1ee8344730d
chihwen <chihwen@doubleservice.com>
2018-04-08 23:04:27 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,20 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /**
+     * @param \App\Models\User $user
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -24,11 +39,15 @@ class UsersController extends Controller
             'email'    => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6',
         ]);
+
         $user = User::create([
             'name'     => $request->name,
             'email'    => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        Auth::login($user);
+
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
         return redirect()->route('users.show', [$user]);
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,17 +2,20 @@
 <html>
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 @include('layouts._header')
 
-<div class="container">
+<div class="container" id="app">
     <div class="col-md-offset-1 col-md-10">
         @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
     </div>
 </div>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
